eb64791179b495d131837aad79011d9b847337eb
Ossama Samir <147268769+EngOssamaSameer@users.noreply.github.com>
2025-04-29 11:21:06 -0700

    Add files via upload

---
+++ Chapter 19/samir.js
@@ -0,0 +1,63 @@
+//comment
+/*
+mlti
+line 
+comment
+*/
+var foo ;
+var number = 5 ;
+var text = "Ossama";
+var nullValue = null;
+var boolValue = true ;
+var arr = [3,"three",6];
+
+
+
+/*
+var test = "testing";
+if(test == "tesdting"){
+    alert("OK");
+}else{
+    alert("NO");
+}*/
+/*
+
+for(var i=0;i<3;i++){
+    alert(arr[i]);
+}*/
+
+
+
+/*
+
+
+function sum(a,b){
+    var c= a+b;
+     alert(c);
+}
+
+var a= 5;
+var b = 6;
+
+sum(a,b);*/
+
+
+
+/*
+
+function sum(a,b){
+    var c= a+b;
+    return c;
+}
+
+var a= 50;
+var b = 6;
+
+alert(sum(a,b));*/
+
+
+function myfun(){
+    alert("Hi JS");
+}
+
+
